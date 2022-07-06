2ae4b807e7a0dc1c4f1ba0a00244547ba233ede5
pkioshima-recordtv <95826765+pkioshima-recordtv@users.noreply.github.com>
2022-07-06 16:43:47 -0300

    Create in-image_adunit_header.js

---
+++ in-image_adunit_header.js
@@ -0,0 +1,8 @@
+src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+
+window.googletag = window.googletag || {cmd: []};
+  googletag.cmd.push(function() {
+    googletag.defineSlot('/7542/TESTE/in-image', [320, 50], 'div-gpt-ad-1657136052116-0').setTargeting('pos', ['in-image']).addService(googletag.pubads());
+    googletag.pubads().enableSingleRequest();
+    googletag.enableServices();
+  });
